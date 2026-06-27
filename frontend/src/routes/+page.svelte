c03e14ce6a3e4ccf4879210eeff7f74ef18ef63c
cinnabar02 <cinnabar02@list.ru>
2026-06-27 23:14:16 +0300

    API_URL_Vercel

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -17,7 +17,8 @@
     let drones = $state(<Drone[]>[]);
 
     onMount(async () => {
-        const response = await fetch("http://localhost:3000/drones");
+        const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+        const response = await fetch(`${API_URL}/drones`);
         drones = await response.json();
     });
 </script>
