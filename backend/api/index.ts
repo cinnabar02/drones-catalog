7de105c92a3657ec66ad541ce9356ac555958668
cinnabar02 <cinnabar02@list.ru>
2026-06-27 23:33:21 +0300

    Ещё одно исправление API

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import { Hono } from 'hono';
-import { serve } from '@hono/node-server'
 import { cors } from 'hono/cors'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma'
@@ -18,9 +17,5 @@ app.get('/drones', async (c) => {
     const drones = await prisma.drone.findMany()
     return c.json(drones)
 });
-
-serve(app, (info) => {
-    console.log(`Server running on http://localhost:${info.port}`)
-})
 
 export default app;
